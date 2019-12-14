823d6d4a7c7f5622cd65ceb9106e24f85836960f
suwat singlor <suwatnolimit@gmail.com>
2019-12-14 22:24:56 +0700

    One piece

--- src/practice/PracticeLab.java
+++ src/practice/PracticeLab.java
@@ -3,5 +3,6 @@ package practice;
 public class PracticeLab {
     public static void main(String... arg){
         System.out.println("Hello Kitty 3");
+        System.out.println("Luufy");
     }
 }
