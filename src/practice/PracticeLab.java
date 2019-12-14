a348bb9fcaaea3c9622848a089addb1760abf0b5
suwat singlor <suwatnolimit@gmail.com>
2019-12-14 22:18:15 +0700

    digmon

--- src/practice/PracticeLab.java
+++ src/practice/PracticeLab.java
@@ -3,5 +3,7 @@ package practice;
 public class PracticeLab {
     public static void main(String... arg){
         System.out.println("Hello Kitty 3");
+
+        System.out.println("Gabumon");
     }
 }
