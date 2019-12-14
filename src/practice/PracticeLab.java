60161427499c24609f911670e66a1e12694cc17a
suwat singlor <suwatnolimit@gmail.com>
2019-12-14 22:12:13 +0700

    3

--- src/practice/PracticeLab.java
+++ src/practice/PracticeLab.java
@@ -2,6 +2,6 @@ package practice;
 
 public class PracticeLab {
     public static void main(String... arg){
-        System.out.println("Hello Kitty");
+        System.out.println("Hello Kitty 3");
     }
 }
